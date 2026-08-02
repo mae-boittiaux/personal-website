761074ab1ba1831d1b709da04a26a737d20acab3
mae-boittiaux <maeboittiaux@gmail.com>
2026-08-02 10:46:07 +0100

    feat: add page-title: 'Mae Boittiaux' to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 style={{ margin: 0, fontSize: "42px" }}>Mae Boittiaux</h1>
+    </>
+  );
 }
 
 export default App;
